843f04b060a2d7a50ae5443dc47e1a4447bc1825
cap <captainphp@yandex.ru>
2019-04-08 23:20:48 +0300

    7_lesson

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,17 +11,55 @@ use App\Service;
 use App\Portfolio;
 use App\People;
 
+use Mail;
+
+use DB;
+
+
 class IndexController extends Controller
 {
     //
-    public function execute(Request $request){
+
+    public function execute(Request $request) {
 
         $pages = Page::all();
         $portfolios = Portfolio::get(array('name','filter','images'));
         $services = Service::where('id','<',20)->get();
         $peoples = People::take(3)->get();
 
+        $tags = DB::table('portfolios')->distinct()->lists('filter');
 
-        return view('site.index');
+
+        $menu = array();
+        foreach($pages as $page) {
+            $item = array('title' =>$page->name,'alias'=>$page->alias);
+            array_push($menu,$item);
+        }
+
+        $item = array('title'=>'Services','alias'=>'service');
+        array_push($menu,$item);
+
+        $item = array('title'=>'Portfolio','alias'=>'Portfolio');
+        array_push($menu,$item);
+
+        $item = array('title'=>'Team','alias'=>'team');
+        array_push($menu,$item);
+
+        $item = array('title'=>'Contact','alias'=>'contact');
+        array_push($menu,$item);
+
+        return view('site.index',array(
+
+            'menu'=> $menu,
+            'pages' => $pages,
+            'services' => $services,
+            'portfolios' => $portfolios,
+            'peoples' => $peoples,
+            'tags'=>$tags
+
+
+
+        ));
     }
+
 }
